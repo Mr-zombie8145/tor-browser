c7b20fa13ff1d35b46044cec8aeb284395b34e50
Georg Koppen <gk@torproject.org>
2020-04-23 09:04:31 +0000

    fixup! Bug 33932: Improve steps for creating gradle deps

--- tools/gen_gradle_deps_file.sh
+++ tools/gen_gradle_deps_file.sh
@@ -31,21 +31,40 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 # Usage:
-# 1) Point to a file with the dependency URLs and specify an output file when
-#    calling the script
-# 2) The output file will contain entries like "sha256sum | URL" or " | URL" for
-#    cases where the resource could not be found (and thus no sha256 sum
-#    computed)
-# 3) $output can then be processed further to generate the final dependencies
-#    file (see: how-to-create-gradle-dependencies-list.txt).
+# 1) Point to a log file with all the dependecy download attempts (for its
+#    generation see: projects/common/how-to-create-gradle-dependencies-list.txt)
+# 2) Double-check that you get the same SHA-256 sums when downloaded from a
+#    different network location. E.g. by using `torsocks` with this script.
 
-urls="$1"
-output="$2"
+log="$1"
+
+# Step 1: Extract all the download attempts out of the log file
+cat $log | grep "Performing HTTP" | grep -o "https://.*" | sort | \
+  uniq > dl-attempts
+
+# Step 2: Fetch all the dependencies and calculate the SHA-256 sum
 while read line
 do
   wget -U "" $line
   fn=$(basename "$line")
   sha256=`sha256sum $fn | cut -d ' ' -f 1`
-  echo "$sha256 | $line" >> $2
+  echo "$sha256 | $line" >> deps
   rm $fn
-done < $urls
+done < dl-attempts
+
+# Step 3: Add the header at the beginning of the final dependency file.
+echo "# On how to update dependencies see projects/common/how-to-create-gradle\
+-dependencies-list.txt" > gradle-dependencies-list.txt
+echo "# Don't forget to update var/gradle_dependencies_version when modifying \
+this file" >> gradle-dependencies-list.txt
+echo "sha256sum | url" >> gradle-dependencies-list.txt
+
+# Step 4: Keep only successfully downloaded artifacts, remove duplicates, and
+# sort based on download URL.
+grep ^[a-f0-9] deps | rev | sort -t/ -u -k1,4 | rev | sort -k 3 >> gradle-dependencies-list.txt
+
+# Step 5: Clean up
+rm dl-attempts
+rm deps
+
+exit 0
